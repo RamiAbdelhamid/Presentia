11720489359560d288c03470723baa4ea703a9eb
Belal Kahaleh <belalkh274@gmail.com>
2025-01-18 20:23:10 +0300

    first commit

---
+++ js/product-details.js
@@ -0,0 +1,21 @@
+import { database } from "./review.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+
+//create user
+function createUser(userId, text) {
+    set(ref (database, 'reviews/' + userId),{
+        text: text,
+    })
+    .then(() => {
+        alert('thanks for your feedback');
+    })
+    .catch((error) => {
+        console.error('Error saving data:', error);
+    });
+}
+document.getElementById('customer-review').addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    const text = document.getElementById('userComment').value;
+    const userId = new Date().getTime().toString();
+    createUser(userId,text);
+});
