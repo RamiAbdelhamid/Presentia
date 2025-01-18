53b6d45bac7e11e23cc02f1cf7e49cdfa627a34f
07asn <hasan.omar.20002@gmail.com>
2025-01-18 19:24:35 +0300

    Hasan Added

--- js/shared/navbar.js
+++ js/shared/navbar.js
@@ -1,7 +1,15 @@
-fetch('../html/shared/navbar.html')
+// (1) Imports
+import { monitorAuthState } from './auth.js';
+
+
+// (2) Function to send data into Firebase
+function loadNavbar(isLoggedIn) {
+        const navbarPath = isLoggedIn ? '../html/shared/logged-navbar.html' : '../html/shared/navbar.html';
+        
+        fetch(navbarPath)
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Failed to fetch navbar.html');
+                    throw new Error('Failed to fetch navbar');
                 }
                 return response.text();
             })
@@ -9,3 +17,10 @@ fetch('../html/shared/navbar.html')
                 document.getElementById('navbar-placeholder').innerHTML = data;
             })
             .catch(error => console.error('Error loading navbar:', error));
+}
+
+
+// (3) Handle Auth State Changes
+monitorAuthState((user) => {
+            loadNavbar(!!user);
+});
