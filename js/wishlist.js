e1ea02947fbfaf27a2f8d6f18b3be0ae8f809a54
ahmma <ahmmadtarawnah@gmail.com>
2025-01-19 09:36:18 +0300

    tarawneh

--- js/wishlist.js
+++ js/wishlist.js
@@ -1,23 +1,10 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDyZX5XE9X5_YLrsiNY6k2oxSPEyLU7Oa4",
-  authDomain: "ahmad-e97be.firebaseapp.com",
-  projectId: "ahmad-e97be",
-  storageBucket: "ahmad-e97be.firebasestorage.app",
-  messagingSenderId: "121003235207",
-  appId: "1:121003235207:web:714704f916ea59b1552522",
-  measurementId: "G-XK1EWE6Q3K",
-};
-
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database(app);
+import { database, ref, set } from "./config.js";
 
 const wishlistTable = document.getElementById("wishlistTable");
 
 function deleteItem(itemKey) {
-  const itemRef = database.ref("Wishlist").child(itemKey);
-
-  itemRef
-    .remove()
+  const itemRef = ref(database, `Wishlist/${itemKey}`);
+  set(itemRef, null)
     .then(() => {
       console.log("Item deleted successfully");
       loadWishlist();
@@ -28,23 +15,18 @@ function deleteItem(itemKey) {
 }
 
 function loadWishlist() {
-  const wishlistRef = database.ref("Wishlist");
+  const wishlistRef = ref(database, "Wishlist");
+
   wishlistRef.once("value", (snapshot) => {
     const wishlist = snapshot.val();
     console.log(wishlist);
 
     wishlistTable.innerHTML = "";
 
-    if (wishlist === null) {
-      // Wishlist collection does not exist
-      wishlistTable.innerHTML =
-        "<tr><td colspan='5' class='text-center'>No products added to your wishlist.</td></tr>";
-    } else if (Object.keys(wishlist).length === 0) {
-      // Wishlist exists but is empty
+    if (wishlist === null || Object.keys(wishlist).length === 0) {
       wishlistTable.innerHTML =
         "<tr><td colspan='5' class='text-center'>No products added to your wishlist.</td></tr>";
     } else {
-      // Wishlist has items, load them
       for (const key in wishlist) {
         const item = wishlist[key];
         const row = `
